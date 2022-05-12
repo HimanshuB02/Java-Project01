2613adbbe049065a71841d0d3fa4147b2832e525
HimanshuB02 <105414004+HimanshuB02@users.noreply.github.com>
2022-05-12 21:10:10 +0530

    Add files via upload

---
+++ form.java
@@ -0,0 +1,8 @@
+public class form {
+    public static void main(String[] args) {
+        String gender ="f";
+        String name = "Jenna";
+        System.out.println("Gender :" +  gender);
+        System.out.println("name: " +name);
+    }
+}
